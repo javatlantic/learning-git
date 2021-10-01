293af809031afec0599ddfc822614ec3b828b8da
rabii <cronaldojosee@gmail.com>
2021-10-01 21:54:06 +0200

    lösche ungültige quellecode

--- utils.java
+++ utils.java
@@ -1,20 +1,14 @@
 
 
-Test 2 to check
-
-
-
-TODD util.java muss implementiert werden
-Implementiert in Main Branch
-
 public class Test {
 
-private String testType;
 
-  
-  private String testfallName;
-  public Test (){
-  }
+ private String testType;
+ private String testfallName;
+
+ public Test (){
+
+ }
 
 }
 
