31ff100d88388348e818454fc5d5b5d6d4a540d4
javatlantic <cronaldojosee@gmail.com>
2021-10-01 20:07:06 +0200

    Update utils.java

--- utils.java
+++ utils.java
@@ -8,7 +8,14 @@ TODD util.java muss implementiert werden
 Implementiert in Main Branch
 
 public class Test {
+
 private String testType;
+
+  
+  private String testfallName;
+  public Test (){
+  }
+
 }
 
 
